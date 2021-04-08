a8df678775caeb99a4dad9de977df51dada71f99
mhubii <m.huber_1994@hotmail.de>
2021-04-08 16:05:12 +0100

    added endoscopy to init

--- python/homography_generators/__init__.py
+++ python/homography_generators/__init__.py
@@ -1,3 +1,4 @@
+from .endoscopy import *
 from .base_homography_generator import BaseHomographyGenerator
 from .calibration_pattern_homography_generator import CalibrationPatternHomographyGenerator
 from .deep_homography_generator import DeepHomographyGenerator
