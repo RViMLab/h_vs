ef5bd6d5230d80118681f89606c77b4ea45e1313
mhubii <m.huber_1994@hotmail.de>
2021-05-17 17:45:06 +0100

    changed feature detector

--- python/homography_generators/stored_view_homography_generator.py
+++ python/homography_generators/stored_view_homography_generator.py
@@ -11,9 +11,9 @@ class StoredViewHomographyGenerator(BaseHomographyGenerator):
     def __init__(self, K: np.ndarray, D: np.ndarray, undistort: bool=False) -> None:
         super().__init__(K, D, buffer_size=None, undistort=undistort)
 
-        # self._feature_detector = cv2.xfeatures2d.SIFT_create()  # proprietary, how to use?
+        self._feature_detector = cv2.xfeatures2d.SIFT_create()  # proprietary, how to use?
         # self._feature_detector = cv2.xfeatures2d.SURF_create()  # proprietary, how to use?
-        self._feature_detector = cv2.ORB_create(nfeatures=2000)
+        # self._feature_detector = cv2.ORB_create(nfeatures=2000)
         # self._feature_detector = cv2.FastFeatureDetector_create()
 
         self._cv_bridge = CvBridge()
@@ -24,17 +24,18 @@ class StoredViewHomographyGenerator(BaseHomographyGenerator):
         img = self._img_graph.nodes[id]['data']
         img = self._cv_bridge.imgmsg_to_cv2(img)
 
-        if self._ud:
-            img0, _ = self.undistort(img0)
-
         # compute homography
-        G, duv = self._feature_homography(img, wrp)
+        G, duv, kp_img, kp_wrp = self._feature_homography(img.astype(np.uint8), wrp.astype(np.uint8), return_kp=True)
         mean_pairwise_distance = None
+
+        # compute mpd
 
         if G is None:
             G = np.eye(3)
 
-        if duv is not None:
-            mean_pairwise_distance = np.linalg.norm(duv, axis=1).mean()
-
+        # if duv is not None:
+        #     mean_pairwise_distance = np.linalg.norm(duv, axis=1).mean()
+        if kp_img is not None and kp_wrp is not None:
+            kp_img, kp_wrp = kp_img.reshape(-1, 2), kp_wrp.reshape(-1, 2)
+            mean_pairwise_distance = np.linalg.norm(kp_img - kp_wrp, axis=1).mean()
         return G, duv, mean_pairwise_distance
