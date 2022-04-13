892d87326ba8d650af38311d1f06173049badf30
mhubii <m.huber_1994@hotmail.de>
2022-04-13 10:20:32 +0100

    added camera intrinsic conversion

--- homography_generators/utils/conversions.py
+++ homography_generators/utils/conversions.py
@@ -2,23 +2,53 @@ import numpy as np
 from std_msgs.msg import MultiArrayLayout, MultiArrayDimension, Float64MultiArray
 
 
-def homographyToMsg(G: np.array) -> Float64MultiArray:
-    if (G.shape != (3, 3)):
-        raise ValueError("Expected (3, 3) Grix, got {}".forG(G.shape))
+def mat3DToMsg(mat: np.array) -> Float64MultiArray:
+    if (mat.shape != (3, 3)):
+        raise ValueError(f"Expected (3, 3) matrix, got {mat.shape}")
     msg = Float64MultiArray(
         layout=MultiArrayLayout(
             dim=[
-                MultiArrayDimension(label="rows", size=G.shape[0]),
-                MultiArrayDimension(label="cols", size=G.shape[1])
+                MultiArrayDimension(label="rows", size=mat.shape[0]),
+                MultiArrayDimension(label="cols", size=mat.shape[1])
             ],
             data_offset=0
         ),
-        data=G.flatten().tolist()
+        data=mat.flatten().tolist()
     )
 
     return msg
 
+def updateCroppedPrincipalPoint(top_left: np.ndarray, K: np.ndarray) -> np.ndarray:
+    r"""Updates the camera's principal point under image cropping.
+    Args:
+        top_left (np.ndarray): Top left corner of cropped image
+        K (np.ndarray): Intrinsic camera parameters
+    Return:
+        K_prime (np.ndarray): Updated intrinsic camera parameters (OpenCV convention)
+    """
+    K_prime = K
+    K_prime[0,2] -= top_left[1]  # cx 
+    K_prime[1,2] -= top_left[0]  # cy
+    return K_prime
+
+
+def updateScaledPrincipalPoint(src_shape: tuple, dst_shape: tuple, K: np.ndarray) -> np.ndarray:
+    r"""Updates the camera's principal point under image scaling.
+    Args:
+        src_shape (tuple): Source shape HxWxC/HxW
+        dst_shape (tuple): Destination shape HxWxC/HxW
+        K (np.ndarray): Intrinsic camera parameters
+    Return:
+        K_prime (np.ndarray): Updated intrinsic camera parameters (OpenCV convention)
+    """
+    K_prime = K
+
+    scale = np.divide(dst_shape[:2], src_shape[:2])
+    K_prime[0,2] *= scale[1]  # cx
+    K_prime[1,2] *= scale[0]  # cy
+    return K_prime
+
 if __name__ == "__main__":
-    G = np.eye(3, 3)
-    msg = homographyToMsg(G)
+    mat = np.eye(3, 3)
+    msg = mat3DToMsg(mat)
     print(msg)
